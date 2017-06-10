05a5e8bf47906fec6172e5bb3033c2bd5fb891e1
Splicid <luis1abreu11@gmail.com>
2017-06-09 22:14:27 -0400

    New Stuff

--- website/script.js
+++ website/script.js
@@ -1,20 +1,68 @@
 $(function () {
+    var config = {
+        apiKey: "AIzaSyBnpzgmDOck_UshUttnOxn0659ciML55wg",
+        authDomain: "website-checker-ebd22.firebaseapp.com",
+        databaseURL: "https://website-checker-ebd22.firebaseio.com",
+        projectId: "website-checker-ebd22",
+        storageBucket: "website-checker-ebd22.appspot.com",
+        messagingSenderId: "1032612384445"
+    };
+    firebase.initializeApp(config);
+   
+    var database = firebase.database();
+
+    var ref = database.ref('websites');
+
+
+    //Input Button
     $('#button').click(function () {
         var input = $('#websiteInput').val()
-        $.ajax(input, {
-            crossDomain: true,
-            dataType: 'jsonp',
-            xhrFields: {
-                withCredentials: true
-            },
-            statusCode: {
-                404: function () {
-                    console.log("Not Working")
-                },
-                200: function () {
-                    console.log("Working")
-                }
+      
+        if (input.substring(0, 7) == "https://") {
+
+        } else {
+            input = "https://" + input;
+        }
+        var data = {
+            website: input,
+        }
+        ref.push(data);
+      
+        ref.on('value', gotData, errData);
+        //function that gets the information from firebase
+        function gotData(data) {
+
+            $(".webs").remove();
+      
+            var websites = data.val();
+            var keys = Object.keys(websites);
+      
+            for (var i = 0; i < keys.length; i++) {
+                var k = keys[i];
+                var website = websites[k].website;
+                $("#data").append('<tr class="webs"> <td class="sites">' + website + "  " +  '<button  class="btn btn-danger btn"><span class="glyphicon glyphicon-remove"></span></button> </td>  </tr>')
             }
-        })
+            //calls the websites
+            $.ajax({
+                url: website,
+                timeout: 3000,
+                statusCode: {
+                    200: function (response) {
+                    },
+                    400: function (response) {
+                        alert('Not working!');
+                    },
+                    0: function (response) {
+                        alert('Not working!');
+                    }
+                }
+            });
+
+
+        }
+        function errData(data) {
+            console.log("error");
+            console.log(err)
+        }
     })
 });
